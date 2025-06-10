ad1dac048ef1d52d6142ddd03c479420880fb007
Ana Priscilla F. <web.anapriscilla@gmail.com>
2025-06-10 00:27:29 -0300

    add minhas praticas em js

---
+++ Basico/03-Alterar-Estilo/script.js
@@ -0,0 +1,12 @@
+
+ function alterarCor() {
+        const cor = document.getElementById("cor");
+        cor.style.backgroundColor = "red";
+}
+
+/* Minhas anotações   
+ Pratiquei:
+ Usar o .style para alterar o estilo css com o js
+ Alterei nesse caso o backgroundColor, mas também posso 
+ alterar o tamanho da fonte (usando fontSize), a cor do texto (usando color)...
+ */
